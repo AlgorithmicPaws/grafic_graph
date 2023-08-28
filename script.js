0d3c11fd354a647cc1ff3d2f73d40ab9c3470df3
racoondev-git <sergio.j.dev@gmail.com>
2023-08-28 01:16:26 -0500

    Changes a index styles y script, add manipulation funtions and hover to see task time and prerequisites. We need to chage the undefined and random codes in name and id, aslo the ede label is needed

--- script.js
+++ script.js
@@ -1,26 +1,91 @@
-// create an array with nodes
-var nodes = new vis.DataSet([
-    { id: 1, label: "Node 1" },
-    { id: 2, label: "Node 2" },
-    { id: 3, label: "Node 3" },
-    { id: 4, label: "Node 4" },
-    { id: 5, label: "Node 5" },
-    ]);
-
-    // create an array with edges
-    var edges = new vis.DataSet([
-    { from: 1, to: 3 },
-    { from: 1, to: 2 },
-    { from: 2, to: 4 },
-    { from: 2, to: 5 },
-    { from: 3, to: 3 },
-    ]);
-
-    // create a network
-    var container = document.getElementById("mynetwork");
-    var data = {
+var nodes = null;
+var edges = null;
+var network = null;
+var data = {
     nodes: nodes,
     edges: edges,
-    };
-    var options = {};
-    var network = new vis.Network(container, data, options);
+  }
+
+function destroy() {
+  if (network !== null) {
+    network.destroy();
+    network = null;
+  }
+}
+
+function draw() {
+  destroy();
+  nodes = [];
+  edges = [];
+
+  var container = document.getElementById("mynetwork");
+  var options = {
+    interaction: { keyboard: true },
+    manipulation: {
+      addNode: function (data, callback) {
+        document.getElementById("operation").innerText = "Add Node";
+        document.getElementById("node-id").value = data.id;
+        document.getElementById("node-label").value = data.label;
+        document.getElementById("node-label").value = data.title;
+        document.getElementById("saveButton").onclick = saveData.bind(this, data, callback);
+        document.getElementById("cancelButton").onclick = clearPopUp.bind();
+        document.getElementById("network-popUp").style.display = "block";
+      },
+      editNode: function (data, callback) {
+        document.getElementById("operation").innerText = "Edit Node";
+        document.getElementById("node-id").value = data.id;
+        document.getElementById("node-label").value = data.label;
+        document.getElementById("saveButton").onclick = saveData.bind(this, data, callback);
+        document.getElementById("cancelButton").onclick = cancelEdit.bind(this, callback);
+        document.getElementById("network-popUp").style.display = "block";
+      },
+      addEdge: function (data, callback) {
+        callback(data)
+      },
+    },
+    nodes: {
+        shape: "dot",
+        size: 20,
+        font: {
+          size: 15,
+          color: "#111",
+        },
+        borderWidth: 2,
+      },
+  };
+  network = new vis.Network(container, data, options);
+}
+
+function clearPopUp() {
+  document.getElementById("saveButton").onclick = null;
+  document.getElementById("cancelButton").onclick = null;
+  document.getElementById("network-popUp").style.display = "none";
+}
+
+function cancelEdit(callback) {
+  clearPopUp();
+  callback(null);
+}
+
+function saveData(data, callback) {
+  data.id = document.getElementById("node-id").value;
+  data.label = document.getElementById("node-label").value;
+  data.title = document.getElementById("node-title").value+ "\n Prerequisite: " +document.getElementById("node-prerequisite").value;
+  document.getElementById("node-id").value = "";
+  document.getElementById("node-label").value = "";
+  document.getElementById("node-title").value = "";
+  document.getElementById("node-prerequisite").value = "";
+  clearPopUp();
+  callback(data);
+}
+
+function init() {
+  draw();
+}
+
+function destroy() {
+  if (network !== null) {
+    network.destroy();
+    network = null;
+  }
+}
